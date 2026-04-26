323a162ac584d71f16deb9b72e76b8b1a0ac270b
SimonGit <15715335111@163.com>
2026-04-27 07:31:45 +0800

    init

--- middleware/cache.go
+++ middleware/cache.go
@@ -2,12 +2,14 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
 func Cache() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		if c.Request.RequestURI == "/" {
-			c.Header("Cache-Control", "no-cache")
+		uri := c.Request.RequestURI
+		if uri == "/" || strings.HasPrefix(uri, "/api") || strings.HasPrefix(uri, "/v1") {
+			c.Header("Cache-Control", "no-store, no-cache, must-revalidate")
 		} else {
 			c.Header("Cache-Control", "max-age=604800") // one week
 		}
